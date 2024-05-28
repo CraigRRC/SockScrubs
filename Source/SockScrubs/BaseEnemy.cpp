02e658ba6ae2bb2d8494ececb786ecadc68cf364
Craig Franceschetti <cfranceschetti33@rrc.ca>
2024-05-28 13:11:58 -0500

    Working on the enemy base. Enemy now has all it's components in place and with the proper attachments.

--- Source/SockScrubs/BaseEnemy.cpp
+++ Source/SockScrubs/BaseEnemy.cpp
@@ -2,13 +2,31 @@
 
 
 #include "BaseEnemy.h"
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SceneComponent.h"
 
 // Sets default values
 ABaseEnemy::ABaseEnemy()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	SetRootComponent(Root);
+	TempBodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	TempBodyMesh->SetupAttachment(Root);
+	BodyHitbox = CreateDefaultSubobject<UBoxComponent>(TEXT("BodyHitbox"));
+	BodyHitbox->SetupAttachment(Root);
+	HeadHitbox = CreateDefaultSubobject<USphereComponent>(TEXT("HeadHitbox"));
+	HeadHitbox->SetupAttachment(Root);
+	TempHeadMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HeadMesh"));
+	TempHeadMesh->SetupAttachment(Root);
+	TempGunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMesh"));
+	TempGunMesh->SetupAttachment(Root);
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSpawn"));
+	ProjectileSpawnPoint->SetupAttachment(TempGunMesh);
 }
 
 // Called when the game starts or when spawned
--- Source/SockScrubs/BaseEnemy.h
+++ Source/SockScrubs/BaseEnemy.h
@@ -6,6 +6,14 @@
 #include "GameFramework/Pawn.h"
 #include "BaseEnemy.generated.h"
 
+UENUM(Blueprintable)
+enum class EnemyState : uint8 {
+	Ready UMETA(DisplayName = "Ready"),
+	Activated UMETA(DisplayName = "Activated"),
+	Combat UMETA(DisplayName = "InCombat"),
+	Stunned UMETA(DisplayName = "Stunned"),
+};
+
 UCLASS()
 class SOCKSCRUBS_API ABaseEnemy : public APawn
 {
@@ -15,15 +23,44 @@ public:
 	// Sets default values for this pawn's properties
 	ABaseEnemy();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	float ActivationRadius{ 16000000.f };
+
+	//Components
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class USphereComponent* HeadHitbox{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class UBoxComponent* BodyHitbox{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class UStaticMeshComponent* TempBodyMesh{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class UStaticMeshComponent* TempHeadMesh{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class USkeletalMeshComponent* TempGunMesh{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class USceneComponent* ProjectileSpawnPoint{};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyAttributes")
+	class USceneComponent* Root{};
+
+	
+
+public:
+	
 
 };
