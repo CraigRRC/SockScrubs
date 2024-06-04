97208862bfd6e32e60c137c5089e52cb369de6f3
Craig Franceschetti <cfranceschetti33@rrc.ca>
2024-06-04 13:39:47 -0500

    Starting on the enemy health widget.

---
+++ Source/SockScrubs/EnemyHealthWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemyHealthWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCKSCRUBS_API UEnemyHealthWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
