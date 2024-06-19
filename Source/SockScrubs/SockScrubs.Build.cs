c99b0420a689822bee05da894e783455dc335bab
Craig Franceschetti <cfranceschetti33@rrc.ca>
2024-06-19 01:28:25 -0500

    made the slate and slate core modules private.

--- Source/SockScrubs/SockScrubs.Build.cs
+++ Source/SockScrubs/SockScrubs.Build.cs
@@ -8,9 +8,9 @@ public class SockScrubs : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
